9aab704e3d0ffe334ecebbbf417fbd964ac19a6e
GitTeaching <aid_aicha@hotmail.fr>
2020-06-24 12:51:50 +0100

    Add files via upload

---
+++ 2 Templates/First/flaskblog.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+posts = [
+	{
+		'author': 'John Doe',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'June 23, 2020'
+	},
+	{
+		'author': 'Jane Doe',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'June 24, 2020'
+	}
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+	return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+	return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
